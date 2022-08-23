72d8efa30aab4fdede604264ee6954115d1e8f36
Peter Lewandowski <mapiotr2010@gmail.com>
2022-08-23 09:59:34 -0400

    added Photo component

---
+++ src/renderer/components/Hello.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+export default function Hello() {
+  return (
+    <div>
+      <div className="Hello">
+        <h1>My Photo Cropper</h1>
+      </div>
+      <div className="Hello">
+        <Link to="/photo">
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Read our docs
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
